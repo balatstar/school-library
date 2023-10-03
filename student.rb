d573bfe8cb74522d0bbbf96206af78a45cdb82d8
joyce <joyce.ann.cruz@gmail.com>
2023-10-03 14:56:54 +0800

    Fix linter errors

--- student.rb
+++ student.rb
@@ -3,8 +3,8 @@ require './person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
-    super(age, name, parent_permission)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
@@ -12,12 +12,3 @@ class Student < Person
     '¯\\(ツ)/¯'
   end
 end
-
-# student1 = Student.new(10, "Gryffindor", "Harry Potter", false)
-# student2 = Student.new(13, "Gryffindor", "George Weasley", true)
-# student3 = Student.new(18, "Gryffindor", "Oliver Wood", false)
-#
-# puts student1.can_use_services?
-# puts student2.can_use_services?
-# puts student3.can_use_services?
-# puts student1.play_hooky
--- teacher.rb
+++ teacher.rb
@@ -12,9 +12,3 @@ class Teacher < Person
     true
   end
 end
-
-# teacher1 = Teacher.new(26, "Charms", "Ms Evans")
-# teacher2 = Teacher.new(45, "Potions", "Professor Snape")
-#
-# puts teacher1.can_use_services?
-# puts teacher2.name
