45afd38b2be9570e46ba568fc372a38c17cacc30
joyce <joyce.ann.cruz@gmail.com>
2023-10-04 00:33:49 +0800

    Fix linter errors

--- capitalize.rb
+++ capitalize.rb
@@ -2,7 +2,6 @@ require './decorator'
 
 class CapitalizeDecorator < Decorator
   def correct_name
-    capitalized_name = @nameable.correct_name.capitalize
-    capitalized_name
+    @nameable.correct_name.capitalize
   end
 end
--- trimmer.rb
+++ trimmer.rb
@@ -2,7 +2,6 @@ require './decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    trimmed_name = @nameable.correct_name[0, 10]
-    trimmed_name
+    @nameable.correct_name[0, 10]
   end
 end
