c0fe8ae9f88d411697380ed033ba6c7ff6d68229
joyce <joyce.ann.cruz@gmail.com>
2023-10-05 21:29:41 +0800

    Create method for main

--- main.rb
+++ main.rb
@@ -8,36 +8,40 @@ $books = []
 
 @classroom_name = Classroom.new('None')
 
-puts 'Welcome to the School Library App!'
+def main
+  puts 'Welcome to the School Library App!'
 
-loop do
-  puts 'Please choose an option by entering a number:'
-  puts '1 - List all books'
-  puts '2 - List all people'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person ID'
-  puts '7 - Exit'
-  choice = gets.to_i
+  loop do
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person ID'
+    puts '7 - Exit'
+    choice = gets.to_i
 
-  case choice
-  when 1
-    list_books($books)
-  when 2
-    list_people($students, $teachers)
-  when 3
-    create_person
-  when 4
-    create_book
-  when 5
-    create_rental($books, $students + $teachers)
-  when 6
-    list_rentals($students + $teachers)
-  when 7
-    puts 'Thank you for using this app!'
-    break
-  else
-    puts 'Invalid choice. Please enter 1 to 7.'
+    case choice
+    when 1
+      list_books($books)
+    when 2
+      list_people($students, $teachers)
+    when 3
+      create_person
+    when 4
+      create_book
+    when 5
+      create_rental($books, $students + $teachers)
+    when 6
+      list_rentals($students + $teachers)
+    when 7
+      puts 'Thank you for using this app!'
+      break
+    else
+      puts 'Invalid choice. Please enter 1 to 7.'
+    end
   end
 end
+
+main
